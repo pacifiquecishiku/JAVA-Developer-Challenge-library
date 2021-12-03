c52c8f69556cc092ef4a9d19ba89bbb2787da16d
Pacifique <pcishiku@gmail.com>
2021-12-03 20:40:09 -0300

    first commit

---
+++ src/main/java/consts/RabbitMQ.java
@@ -0,0 +1,10 @@
+package consts;
+
+public class RabbitMQ {
+
+    public static final String DEFAULT_EXCHANGE = "amq.direct";
+    public static final String SUM_QUEUE = "sum";
+    public static final String SUBTRACTION_QUEUE = "subtraction";
+    public static final String MULTIPLICATION_QUEUE = "multiplication";
+    public static final String DIVISION_QUEUE = "division";
+}
